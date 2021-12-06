8d4b44debad0c2fd319a5115bdd5f242ddde7330
kitchen-boy <kaye.chen.boyce@gmail>
2021-12-06 15:37:07 -0500

    Generate the HTML template and file

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+//profileDataArgs array
+const profileDataArgs = process.argv.slice(2);
+//console.log(profileDataArgs);
+//use the array index with the following expressions
+const [name, github] = profileDataArgs;
 
+/* 9.2.3 - commented all code that was done in previous lesson 9.1
 const printProfileData = (profileDataArr) => {
-    /*This...*/
+    //This...
     for (let i = 0; i < profileDataArr.length; i++) {
         console.log(profileDataArr[i]);
     }
@@ -11,6 +17,14 @@ const printProfileData = (profileDataArr) => {
     // Is the same as this...
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
+printProfileData(profileDataArgs);*/
 
-
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    /*In the callback function block, a conditional statement checks 
+    for the err being returned by the callback function. 
+    If err exists, an error message is displayed, using the following statement:*/
+    if (err) throw new Error (err);
+    
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
