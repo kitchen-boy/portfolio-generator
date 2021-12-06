2ff1450075c57c3bc0c483ecc43ae2ad9bb3e593
kitchen-boy <kaye.chen.boyce@gmail>
2021-12-06 17:56:09 -0500

    Use Inquirer

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-//profileDataArgs array
-const profileDataArgs = process.argv.slice(2);
-//console.log(profileDataArgs);
-//use the array index with the following expressions
-const [name, github] = profileDataArgs;
+const inquirer = require ('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-/* 9.2.3 - commented all code that was done in previous lesson 9.1
-const printProfileData = (profileDataArr) => {
-    //This...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    console.log ('=================');
+// const pageHTML = generatePage(name, github);
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-printProfileData(profileDataArgs);*/
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    /*In the callback function block, a conditional statement checks 
-    for the err being returned by the callback function. 
-    If err exists, an error message is displayed, using the following statement:*/
-    if (err) throw new Error (err);
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw (err);
     
-  
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
