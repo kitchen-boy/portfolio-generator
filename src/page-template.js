c2816c6e2c44abfb16a34ec344950fad972dd21d
kitchen-boy <kaye.chen.boyce@gmail>
2021-12-08 16:57:57 -0500

    Conditionally create the about section HTML

--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,17 @@
+// create the about section
+const generateAbout = aboutText => {
+    if (!aboutText) {
+      return '';
+    }
+  
+    return `
+      <section class="my-3" id="about">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+        <p>${aboutText}</p>
+      </section>
+    `;
+  };
+
 module.exports = templateData => {  
     // destructure page data by section
     // this will create three variables based on data in templateData
@@ -26,7 +40,7 @@ module.exports = templateData => {
             </div>
         </header>
         <main class="container my-5">
-
+            ${generateAbout(about)}
         </main>
         <footer class="container text-center py-3">
             <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
