413242685b89abecb00170da7a39f475d7098e67
kitchen-boy <kaye.chen.boyce@gmail>
2021-12-10 00:26:27 -0500

    Used .map() method for projectArr array

--- src/page-template.js
+++ src/page-template.js
@@ -12,6 +12,50 @@ const generateAbout = aboutText => {
     `;
   };
 
+const generateProjects = projectsArr => {
+    // get array of just featured projects
+    const featuredProjects = projectsArr.filter(project => {
+        if (project.feature) {
+            return true;
+        }   else {
+            return false;
+        }
+    });
+
+    // get array of all non-featured projects
+    const nonFeaturedProjects = projectsArr.filter(project => {
+        if (!project.feature) {
+            return true;
+        }   else {
+            return false;
+        }
+    });
+    
+    const projectHtmlArr = projectsArr.map(({ name, description, languages, link }) => {
+        return `
+            <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+                <h3 class="portfolio-item-title text-light">${name}</h3>
+                <h5 class="portfolio-languages">
+                    Built With:
+                    ${languages.join(', ')}
+                </h5>
+                <p>${description}</p>
+                <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            </div>
+            `;
+        });
+
+        return `
+            <section class="my-3" id="portfolio">
+                <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+                <div class="flex-row justify-space-between">
+                <!-- Leaving this empty as we'll dynamically insert project HTML here -->
+                ${projectHtmlArr.join('')}
+                </div>
+            </section>
+        `;
+};  
+
 module.exports = templateData => {  
     // destructure page data by section
     // this will create three variables based on data in templateData
@@ -41,6 +85,7 @@ module.exports = templateData => {
         </header>
         <main class="container my-5">
             ${generateAbout(about)}
+            ${generateProjects(projects)}
         </main>
         <footer class="container text-center py-3">
             <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
