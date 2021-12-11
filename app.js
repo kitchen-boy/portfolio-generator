b8e81cc269f04f9b6b45fc6b5fab606f364730ba
kitchen-boy <kaye.chen.boyce@gmail>
2021-12-11 00:35:35 -0500

    Update fs.writeFile() method

--- app.js
+++ app.js
@@ -188,10 +188,18 @@ const mockData =
 
       const pageHTML = generatePage(mockData);      
 
-   fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-     console.log('Page created! Check out index.html in this directory to see it!');
-     });
-    
-  //});
+fs.writeFile('./dist/index.html', pageHTML, err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Page created! Check out index.html in this directory to see it!');
+      
+  fs.copyFile('./src/style.css', './dist/style.css', err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('Style sheet copied successfully!');
+  });
+});   
